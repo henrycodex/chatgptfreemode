89d87327eff7a79a3fcb2a73b05d8eb8b834e1fe
henrycodex <124052219+henrycodex@users.noreply.github.com>
2023-01-31 15:39:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
+import firebase from 'firebase/compat/app';
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
 import streamlit as st
 from streamlit_chat import message
 
