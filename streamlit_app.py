c0bf2a319625285b6205e23ab16c0db3a0098a79
henrycodex <124052219+henrycodex@users.noreply.github.com>
2023-02-01 21:49:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,8 +70,8 @@ if user_input:
     history_input.append([user_input, output])
     st.session_state.past.append(user_input)
     st.session_state.generated.append(output[0])
-
 if st.session_state['generated']:
+
     for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state["generated"][i], is_user=False, key=str(i))
+        message(st.session_state["generated"][i], key=str(i))
         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
