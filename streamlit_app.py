689535a3d94b13183675347a8ea34c457128a867
henrycodex <124052219+henrycodex@users.noreply.github.com>
2023-02-01 22:44:19 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,34 +5,27 @@ import os
 
 openai.api_key = os.getenv("OPEN_API_KEY")
 
-# openAI code
 def openai_create(prompt):
-
     response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=prompt,
-    temperature=0.5,
-    max_tokens=1000,
-    top_p=1,
-    frequency_penalty=0.1,
-    presence_penalty=0.1,
-    stop=[" Human:", " TextWizardAI:"]
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=1000,
+        top_p=1,
+        frequency_penalty=0.1,
+        presence_penalty=0.1,
+        stop=[" Human:", " TextWizardAI:"]
     )
 
     return response.choices[0].text
 
-
-def chatgpt_clone(input, history):
+def chatgpt_clone(input_, history=None):
     history = history or []
-    s = list(sum(history, ()))
-    print(s)
-    s.append(input)
-    inp = ' '.join(s)
+    history.append(input_)
+    inp = ' '.join(history)
     output = openai_create(inp)
-    history.append((input, output))
-    return history, history
+    return output
 
-# Streamlit App
 st.set_page_config(
     page_title="TextWizardAI Chatbot",
     page_icon=":robot:"
@@ -40,30 +33,24 @@ st.set_page_config(
 
 st.header("TextWizardAI Chatbot")
 
-history_input = []
-
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
 
 if 'past' not in st.session_state:
     st.session_state['past'] = []
 
-
 def get_text():
     input_text = st.text_input("You: ", key="input")
-    return input_text 
-
+    return input_text
 
 user_input = get_text()
 
-
 if user_input:
-    output = chatgpt_clone(user_input, history_input)
-    history_input.append([user_input, output])
+    output = chatgpt_clone(user_input)
     st.session_state.past.append(user_input)
-    st.session_state.generated.append(output[0])
-if st.session_state['generated']:
+    st.session_state.generated.append(output)
 
+if st.session_state['generated']:
     for i in range(len(st.session_state['generated'])-1, -1, -1):
         message(st.session_state["generated"][i], key=str(i))
         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
